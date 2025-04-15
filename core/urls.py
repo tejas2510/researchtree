1db5cde0dba24cb3aa15d78f28b0b03c97d8f4e2
Tejas Patil <72490903+tejas2510@users.noreply.github.com>
2025-04-15 23:05:16 +0530

    Update urls.py

--- core/urls.py
+++ core/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('about/', views.about, name='about'),
+    path('create-superuser/', create_superuser, name='create_superuser'),
 ]
