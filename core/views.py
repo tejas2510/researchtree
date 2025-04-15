75177de8cfc10b8b21f0bea2c8f2c49d5f2d4dd5
Tejas Patil <72490903+tejas2510@users.noreply.github.com>
2025-04-15 23:04:53 +0530

    Update views.py

--- core/views.py
+++ core/views.py
@@ -1,7 +1,16 @@
 from django.shortcuts import render
-
+from django.core.management import call_command
+from django.http import HttpResponse
 def home(request):
     return render(request, 'home.html')
 
 def about(request):
     return render(request, 'about.html')
+    
+def create_superuser(request):
+    try:
+        # Replace with your superuser credentials
+        call_command('createsuperuser', '--username', 'admin', '--email', 'admin@example.com', '--noinput')
+        return HttpResponse("Superuser created successfully!")
+    except Exception as e:
+        return HttpResponse(f"Error: {e}")
